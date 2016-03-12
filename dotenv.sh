da33d6ad99b4556b1879a27a678c27113f555dd6
Jason Leung <jason@madcoda.com>
2016-03-12 18:08:53 +0800

    test if .env exists

--- dotenv.sh
+++ dotenv.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
+
 # inject .env configs into the shell
-export $(cat .env | grep -v ^# | xargs)
+if [ -f .env ]; then
+	export $(cat .env | grep -v ^# | xargs)
+else
+	echo ".env file not found"
+fi
 
 # then run whatever commands you like
 exec "$@"
