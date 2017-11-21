4fc54d6a14438c6fd8603370b7ae664ab26b6730
Patrick Way <patrick.way@intersection.com>
2017-11-21 18:54:15 -0500

    Test blank lines are ok.  Test existing ENV variables are skipped.  Test last line of the file is parsed even if newline is omitted.

--- tests/dotenv-test.sh
+++ tests/dotenv-test.sh
@@ -4,13 +4,16 @@
 main() {
 
     cd "$( dirname "${BASH_SOURCE[0]}" )" || exit 1
-
+    
     # shellcheck disable=SC1091
+    export TEST_EXISTING="expected"
     source ../dotenv.sh
     assert_equal "$TEST_UNQUOTED" 'a=1 b=2 c=3' 'Testing unquoted'
     assert_equal "$TEST_SINGLE_QUOTED" '1 2 3 4' 'Testing single quoted'
     assert_equal "$TEST_DOUBLE_QUOTED" '1 2 3 4' 'Testing double quoted'
     assert_equal "$TEST_INTERPOLATION" 'a=1 b=2 c=3 d=4' 'Testing interpolation'
+    assert_equal "$TEST_EXISTING" 'expected' 'Testing preservation of existing variables'
+    assert_equal "$TEST_NO_NEWLINE" 'still there' 'Testing parsing of last line'
 }
 
 assert_equal() {
