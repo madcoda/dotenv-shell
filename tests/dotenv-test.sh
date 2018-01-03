f4221d03e0bf7476b6ef48a525f75b58ee1f60e6
Patrick Way <patrick.way@intersection.com>
2018-01-02 19:09:23 -0500

    Update test invocation to chain defaults and local secrets

--- tests/dotenv-test.sh
+++ tests/dotenv-test.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-
 main() {
-
     cd "$( dirname "${BASH_SOURCE[0]}" )" || exit 1
-    
+    export -f assert_equal
+
     # shellcheck disable=SC1091
     export TEST_EXISTING="expected"
-    source ../dotenv.sh
-    echo "Testing blank line parsing: ok" # i.e. didn't crash
-    assert_equal "$TEST_UNQUOTED" 'a=1 b=2 c=3' 'Testing unquoted'
-    assert_equal "$TEST_SINGLE_QUOTED" '1 2 3 4' 'Testing single quoted'
-    assert_equal "$TEST_DOUBLE_QUOTED" '1 2 3 4' 'Testing double quoted'
-    assert_equal "$TEST_INTERPOLATION" 'a=1 b=2 c=3 d=4' 'Testing interpolation'
-    assert_equal "$TEST_EXISTING" 'expected' 'Testing preservation of existing variables'
-    assert_equal "$TEST_NO_NEWLINE" 'still there' 'Testing parsing of last line'
+    ../dotenv.sh DOTENV_FILE=another.env ../dotenv.sh ./test-assertions.sh
 }
 
 assert_equal() {
