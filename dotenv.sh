05e84ada4833f6473b2a78680309875bb3fea4a9
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2019-01-03 09:22:37 -0300

    Allow envvar overwrite
    Sometimes the user would like to overwrite a environment variable that
    exists on the user configuration, like the HOME, GOPATH or something
    else.
    
    This PR will load the DOTENV_DEFAULT first and after that will load the
    DOTENV_FILE.

--- dotenv.sh
+++ dotenv.sh
@@ -54,14 +54,20 @@ export_envs() {
 			continue
 		fi
 
-		if is_set "$key"; then
-			log_verbose "Existing: $key=$val"
-		else
-			value=$(eval echo "$temp")
-			eval export "$key='$value'";
-		fi
+        value=$(eval echo "$temp")
+        eval export "$key='$value'";
 	done < $1
 }
+
+# inject any defaults into the shell
+if is_set "DOTENV_DEFAULT"; then
+	log_verbose "Setting defaults via $DOTENV_DEFAULT"
+	if [ -f "$DOTENV_DEFAULT" ]; then
+		export_envs "$DOTENV_DEFAULT"
+	else
+		echo '$DOTENV_DEFAULT file not found'
+	fi
+fi
 
 if is_set "DOTENV_FILE"; then
 	log_verbose "Reading from $DOTENV_FILE"
@@ -76,15 +82,6 @@ else
 	echo "$DOTENV_FILE file not found"
 fi
 
-# inject any defaults into the shell
-if is_set "DOTENV_DEFAULT"; then
-	log_verbose "Setting defaults via $DOTENV_DEFAULT"
-	if [ -f "$DOTENV_DEFAULT" ]; then
-		export_envs "$DOTENV_DEFAULT"
-	else
-		echo '$DOTENV_DEFAULT file not found'
-	fi
-fi
 
 # then run whatever commands you like
 if [ $# -gt 0 ]; then
